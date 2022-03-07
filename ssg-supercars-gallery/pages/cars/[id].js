602ce8d9c3276d64113767bbba93c31f27428570
rbiedrawa <biedrawa.rafal@gmail.com>
2022-03-07 08:23:46 +0100

    feat(SSG): Initial SSG example application configuration (copy of SSR app)

---
+++ ssg-supercars-gallery/pages/cars/[id].js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import {Container, Row} from "react-bootstrap";
+
+const fetchCar = async (id) => {
+    const { data } = await axios.get(`http://localhost:3002/api/cars?id=${escape(id)}`);
+    return data;
+};
+
+export async function getServerSideProps(context) {
+    const data = await fetchCar(context.params.id);
+    return {
+        props: {
+            data: data,
+        }, // will be passed to the page component as props
+    }
+}
+
+
+const Car = ({data}) => {
+    return (
+        <div>
+            <Container>
+                {data && (
+                    <>
+                        <Row>
+                            <img
+                                src={`/cars/${data.id}.jpg`}
+                                style={{
+                                    width: "100%",
+                                }}
+                            />
+                        </Row>
+                        <Row>
+                            <h1 className="text-center">{(data && data.name) || "SuperCar"}</h1>
+                        </Row>
+                    </>
+                )}
+            </Container>
+        </div>
+    );
+}
+
+export default Car;
